4610abd037d6d864f882d4e069f4229ee8d1c5bd
Tim Sullivan <timaaarrreee@gmail.com>
2011-09-20 20:44:14 +1200

    First upload

---
+++ src/nz/co/centralweb/wificargui/Main.java
@@ -0,0 +1,11 @@
+package nz.co.centralweb.wificargui;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		//begin the awesomeness here.
+		System.out.println("Hello");
+		
+	}
+
+}
